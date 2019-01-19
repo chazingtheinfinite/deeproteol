2db65fc1a993ac23c07960066de09797f2e04ffd
Kevin Dick <kevin.dick9@gmail.com>
2019-01-19 12:43:42 -0500

    Added cardinal points to the icosagon image...

--- code/icosagon_cgr.py
+++ code/icosagon_cgr.py
@@ -15,7 +15,19 @@ icodict = mm.create_icosagon_dict(RADIUS)
 print(icodict)
 
 # Create a Blank Image with Dimensions (2r, 2r)
-img = Image.new('RGBA', (2*RADIUS, 2*RADIUS), color=255)
+img = Image.new('RGBA', (2*RADIUS, 2*RADIUS), color=(255, 255,255,255))
+
+#for i in range(img.size[0]): # for every pixel:
+#    for j in range(img.size[1]):
+#        print(f'{(i,j)} : {img.getpixel((i,j))}')
+
+# TODO:: Create definition for setting a patch around the pixel to make obvious points...
+
+pixels = img.load()
+for aa in icodict.keys():
+    i,j = icodict[aa]
+    print(f'Setting black to {(i,j)}: {aa}')
+    pixels[i-1,j-1] = (0,0,0,255)
+
+
 img.show()
-for px in img:
-    print(img.getpixel(px))
